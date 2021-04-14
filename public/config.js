451713abea4727695c29586d7f2b46ba6c57f75f
AbdulRehman197 <38237505+AbdulRehman197@users.noreply.github.com>
2021-04-14 22:54:49 +0500

    add config

--- public/config.js
+++ public/config.js
@@ -1,14 +1,12 @@
 (function () {
-
-  var loginDomain = 'localhost';
-  var wsURL = "ws://localhost:7070/ws/";
+  // var loginDomain = "http://ec2-3-141-65-53.us-east-2.compute.amazonaws.com/http-bind";
+  var wsURL = "wss://ws.xabber.com:9443/websocket";
 
   window.config = {
     xmpp: {
       transport: "websocket",
-      wsURL: wsURL
-    },
-    xmppDomain: loginDomain,
+      wsURL: wsURL,
+    }
+    // xmppDomain: loginDomain,
   };
-  
 })();
--- src/sagas/xmpp/setupClient.js
+++ src/sagas/xmpp/setupClient.js
@@ -5,9 +5,11 @@ function setupClient() {
   const client = XMPP.createClient({
     sasl: ["scram-sha-1", "cram-md5", "digest-md5"],
     ...window.config.xmpp
+    // capsNode: "https://web.xabber.com"
   });
 
   window.client = client;
+  console.log("window", window)
   configureClient(client);
 
   client.on("session:started", () => {
